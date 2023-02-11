e466713ffc2ea813ac147652763c25c8a5950edd
ana <anapaulac@live.com>
2023-02-10 21:53:25 -0300

    show pokemon details

--- src/components/BuscarPokemon.vue
+++ src/components/BuscarPokemon.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="BuscarPokemon">
+  <div class="container">
     <b-row class="w-100">
-
       <b-col cols="12">
         <b-form @submit.prevent="onSubmit">
           <b-form-group
@@ -15,27 +14,24 @@
                 id="input-1"
                 v-model="name"
                 type="text"
-              ></b-form-input>              
+              ></b-form-input>
             </div>
           </b-form-group>
 
-          <b-button
-            variant="info"
-            type="submit"
-            >Buscar</b-button
-          >
+          <b-button variant="info" type="submit">Buscar</b-button>
         </b-form>
       </b-col>
     </b-row>
 
     <b-row v-if="Object.keys(evolutions).length !== 0" class="w-100 mt-2">
       <b-col cols="12">
-
-        <b-table striped hover :items="evolutions" :fields="fields" >
+        <b-table striped hover :items="evolutions" :fields="fields">
           <template v-slot:cell(picture)="row">
-            <img v-if="row.item.picture"
+            <img
+              v-if="row.item.picture"
               :src="row.item.picture"
-              title="contact-img" class="avatar-sm"
+              title="contact-img"
+              class="avatar-sm"
             />
           </template>
           <template v-slot:cell(actions)="row">
@@ -43,69 +39,71 @@
               Ver mais
             </b-button>
           </template>
-
         </b-table>
-
       </b-col>
     </b-row>
-
   </div>
 </template>
 
 <script>
-import { getPokemonName, pokemonSpecies, apiUrlCall } from '../api/api-pokemon.js'
+import {
+  getPokemonName,
+  pokemonSpecies,
+  apiUrlCall,
+} from "../api/api-pokemon.js";
 
 export default {
-  name: 'BuscarPokemon',
-  props: {
-  },
-  data(){
+  name: "BuscarPokemon",
+  props: {},
+  data() {
     return {
-      name: '',
+      name: "",
       pokemons: [],
       evolutions: [],
-      item: [{ id: 40, name: 'Dickerson', picture: 'Macdonald' }],
+      item: [{ id: 40, name: "Dickerson", picture: "Macdonald" }],
       fields: [
-        { key: 'name', label: 'Nome do Pokemon'},
-        { key: 'picture', label: 'Imagem'},
-        { key: 'actions', label: 'Ação' },
+        { key: "name", label: "Nome do Pokemon" },
+        { key: "picture", label: "Imagem" },
+        { key: "actions", label: "Ação" },
       ],
-    }
+    };
   },
   methods: {
-    async setPokemon(){
-      const resp = await getPokemonName(this.name)      
-      if(resp.status !== 200 ) {
-        this.$toast.warning(`Erro: ${resp.message} `)
-        return
-      } else 
-      this.evolutions.push({
-        name: resp.message.name,
-        picture: resp.message.sprites.front_default
-      })
+    async setPokemon() {
+      const resp = await getPokemonName(this.name);
+      if (resp.status !== 200) {
+        this.$toast.warning(`Erro: ${resp.message} `);
+        return;
+      } else
+        this.evolutions.push({
+          name: resp.message.name,
+          picture: resp.message.sprites.front_default,
+        });
     },
     onSubmit(event) {
-      event.preventDefault()    
-      this.evolutions = []  
-      if (!this.name) return 
-      this.setPokemon()
-      this.getEvolutions()
+      event.preventDefault();
+      this.evolutions = [];
+      if (!this.name) return;
+      this.setPokemon();
+      this.getEvolutions();
     },
-    async getEvolutions(){      
-      const species = await pokemonSpecies(this.name)      
-      const evolutionsUrl = species.message.evolution_chain.url
+    async getEvolutions() {
+      const species = await pokemonSpecies(this.name);
+      const evolutionsUrl = species.message.evolution_chain.url;
 
-      const resp = await apiUrlCall(evolutionsUrl)  
-      const evolutions = resp.message.chain.evolves_to
+      const resp = await apiUrlCall(evolutionsUrl);
+      const evolutions = resp.message.chain.evolves_to;
 
-      evolutions.map(e => this.evolutions.push({
-        name: e.species.name,
-        picture: ''
-      }))   
+      evolutions.map((e) =>
+        this.evolutions.push({
+          name: e.species.name,
+          picture: "",
+        })
+      );
     },
-    seeDetails(name){
-      this.$router.push({ path: `/pokemon/${name}` })
-    }
-  }
-}
+    seeDetails(name) {
+      this.$router.push({ path: `/pokemon/${name}` });
+    },
+  },
+};
 </script>
--- src/components/PokemonDetails.vue
+++ src/components/PokemonDetails.vue
@@ -1,54 +1,80 @@
 <template>
-  <div class="PokemonDetails">   
-    {{pokemon}}
-    <b-row v-if="Object.keys(pokemon).length !== 0" class="w-100">
-      <b-col cols="12">
-        <b-card
-          :title="pokemon.name"
-          :img-src="pokemon.picture"
-          img-alt="Image"
-          img-top
-          tag="article"
-          style="max-width: 20rem;"
-          class="text-center mb-2"
-        >
-        </b-card>
-      </b-col>
-    </b-row>
+  <div class="container">
+    <b-card no-body class="overflow-hidden" style="max-width: 1000px">
+      <b-row no-gutters>
+        <b-col md="6">
+          <b-card-img
+            :src="pokemon.picture"
+            alt="Image"
+            class="rounded-0"
+          ></b-card-img>
+        </b-col>
+        <b-col md="6">
+          <b-card-body>
+            <b-card-text>
+              <h4 class="font-weight-bold">NOME POKEMON: {{ pokemon.name }}</h4>
 
+              <b-table
+                striped
+                hover
+                :items="pokemon.properties"
+                :fields="fields"
+              >
+                <template v-slot:cell(name)="row">
+                  {{ row.item.name }}
+                </template>
+              </b-table>
+            </b-card-text>
+          </b-card-body>
+        </b-col>
+      </b-row>
+    </b-card>
   </div>
 </template>
 
 <script>
-import { getPokemonName } from '../api/api-pokemon.js'
+import { getPokemonName } from "../api/api-pokemon.js";
 
 export default {
-  name: 'PokemonDetails',
-  props: {
-  },
-  data(){
+  name: "PokemonDetails",
+  props: {},
+  data() {
     return {
       pokemon: [],
-    }
+      fields: [
+        { key: "prop", label: "Nome da propriedade" },
+        { key: "value", label: "Valor" },
+        { key: "effort", label: "Esforço" },
+      ],
+    };
   },
   async mounted() {
-    const resp = await getPokemonName(this.$route.params.name)  
-    
-    if(resp.status !== 200 ) {
-        this.$toast.warning(`Erro: ${resp.message} `)
-        return
+    const resp = await getPokemonName(this.$route.params.name);
+
+    if (resp.status !== 200) {
+      this.$toast.warning(`Erro: ${resp.message} `);
+      return;
     }
-    this.setPokemon(resp.message)
+    this.setPokemon(resp.message);
   },
   methods: {
-    async setPokemon(item){    
-      console.log(item)
+    async setPokemon(item) {
       this.pokemon = {
         name: item.name,
-        picture: item.sprites.front_default
-      }
-      // this.pokemon = item
-    }
-  }
-}
+        picture: item.sprites.front_default,
+        properties: item.stats.map((m) => ({
+          prop: m.stat.name,
+          value: m.base_stat,
+          effort: m.effort,
+        })),
+      };
+    },
+  },
+};
 </script>
+
+<style lang="scss">
+.container {
+  text-align: center;
+}
+</style>
